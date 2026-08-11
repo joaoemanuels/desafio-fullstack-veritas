712dfd81ccc4f808338b87061746f3db0c8575cc
joaoemanuels <jemanuel.pi@gmail.com>
2026-08-11 12:06:48 -0300

    feat: configuração da URL da API por ambiente

--- backend/main.go
+++ backend/main.go
@@ -7,9 +7,10 @@ import (
 
 func withCORS(next http.Handler) http.Handler {
 	allowedOrigins := map[string]bool{
-		"http://localhost:5173":                        true,
-		"http://192.168.0.6:5173":                      true,
-		"https://desafio-fullstack-veritas.vercel.app": true,
+		"http://localhost:5173":                              true,
+		"http://localhost:4173":                              true,
+		"http://192.168.0.6:5173":                            true,
+		"https://desafio-fullstack-veritas-c82j.vercel.app/": true,
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
